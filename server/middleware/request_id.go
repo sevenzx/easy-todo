4d1421124c101510ebf85ac540446a01ec362860
xuan <zx786@vip.qq.com>
2024-07-15 01:53:01 +0800

    feat: requestId仅仅使用uuid

--- server/middleware/request_id.go
+++ server/middleware/request_id.go
@@ -2,20 +2,17 @@ package middleware
 
 import (
 	"easytodo/global/consts"
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 	"strings"
-	"time"
 )
 
 func RequestId() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		if requestId := c.Request.Header.Get(consts.RequestIdKey); requestId == "" {
-			s := uuid.New().String()
-			s = strings.ReplaceAll(s, "-", "")
-			s = strings.ToUpper(s)
-			requestId = fmt.Sprintf("%s%s", time.Now().Format("20060102150405"), s)
+			requestId = uuid.New().String()
+			requestId = strings.ReplaceAll(requestId, "-", "")
+			requestId = strings.ToUpper(requestId)
 			c.Request.Header.Set(consts.RequestIdKey, requestId)
 		}
 		c.Next()
