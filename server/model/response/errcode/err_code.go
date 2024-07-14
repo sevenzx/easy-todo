1543c28ad7334a2dcef7ccd765ac67198193ba5b
xuan <zx786@vip.qq.com>
2024-07-15 01:52:18 +0800

    feat: 定义错误码

---
+++ server/model/response/errcode/err_code.go
@@ -0,0 +1,18 @@
+package errcode
+
+type ErrCode int
+
+// 1. 安装stringer工具: go install golang.org/x/tools/cmd/stringer
+// 2. 定义好ErrCode以及Message之后，运行以下命令自动生成新的错误码和错误信息
+//go:generate stringer -type ErrCode -linecomment
+
+const Success ErrCode = 0 // success
+
+// 1开头：服务级错误码
+const (
+	// ServerError 内部错误
+	ServerError     ErrCode = iota + 10001 // 服务内部错误
+	ParamsError                            // 参数信息有误
+	TokenAuthFail                          // Token鉴权失败
+	TokenIsNotExist                        // Token不存在
+)
